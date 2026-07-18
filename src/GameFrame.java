02539f8f7a83607398e94b0d6f02a4fb1976c235
Wade Helquist <whelqui@wgu.edu>
2026-07-18 10:42:18 -0600

    Prepare StrataSnake for browser deployment

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,9 +5,11 @@ public class GameFrame extends JFrame {
         this.add(new  GamePanel());
         this.setTitle("Sneggy in StrataSnake");
         this.setResizable(false);
+        this.setUndecorated(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
+        this.setLocation(0, 0);
         this.setVisible(true);
-        this.setLocationRelativeTo(null);
 
     }
 }
